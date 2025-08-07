6131debf9b754ed741d01011a6684405f5da36f7
moscowxy <160858163+moscowxy@users.noreply.github.com>
2025-08-07 19:26:29 +0300

    install_blockassist.sh

--- install_blockassist.sh
+++ install_blockassist.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-# BlockAssist otomatik kurulum scripti
+echo "[1/8] BlockAssist setup başlatılıyor..."
 
-echo "[2/8] Setup scripti çalıştırılıyor..."
+echo "[2/8] setup.sh çalıştırılıyor..."
+chmod +x ./setup.sh
 ./setup.sh
 
-echo "[3/8] Pyenv kuruluyor..."
+echo "[3/8] pyenv kuruluyor..."
 curl -fsSL https://pyenv.run | bash
 
-echo "kalıcı hale getiriliyor..."
-echo -e '\nexport PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo -e '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo -e 'eval "$(pyenv init - bash)"' >> ~/.bashrc
-echo -e 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-source ~/.bashrc
-
-echo "[4/8] Python bağımlılıkları yükleniyor..."
-sudo apt update
-sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
-libsqlite3-dev curl git libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
-libffi-dev liblzma-dev
-
-pyenv install 3.10
-pip install psutil readchar
-
-export PATH="$HOME/.pyenv/bin:$PATH"
+# pyenv ortam değişkenlerini yükle
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-echo "[5/8] cuDNN kuruluyor..."
+echo "[4/8] Python 3.10 kuruluyor..."
+sudo apt update
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
+  libsqlite3-dev curl git libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
+  libffi-dev liblzma-dev
+
+pyenv install 3.10.14
+pyenv global 3.10.14
+
+echo "[5/8] pip, psutil ve readchar kuruluyor..."
+pip install --upgrade pip
+pip install psutil readchar
+
+echo "[6/8] CUDNN indiriliyor ve kuruluyor..."
 wget https://developer.download.nvidia.com/compute/cudnn/9.11.0/local_installers/cudnn-local-repo-ubuntu2204-9.11.0_1.0-1_amd64.deb
 sudo dpkg -i cudnn-local-repo-ubuntu2204-9.11.0_1.0-1_amd64.deb
 sudo cp /var/cudnn-local-repo-ubuntu2204-9.11.0/cudnn-local-4EC753EA-keyring.gpg /usr/share/keyrings/
@@ -36,12 +36,13 @@ echo "deb [signed-by=/usr/share/keyrings/cudnn-local-4EC753EA-keyring.gpg] file:
 sudo apt update
 sudo apt install -y libcudnn9 libcudnn9-dev
 
+# Cuda path export
 echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
 source ~/.bashrc
 
-echo "[6/8] Python başlatılıyor..."
-sudo apt update
-sudo apt install -y python3.12-venv
-pip install readchar
-pip install psutil
+echo "[7/8] run.py çalıştırma komutu hazırlanıyor..."
 
+echo "[8/8] Kurulum tamamlandı. run.py'yi aşağıdaki komutla başlatabilirsin:"
+echo ""
+echo "python run.py"
+echo ""
