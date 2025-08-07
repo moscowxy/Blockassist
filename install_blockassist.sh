71d9442fd8f46e67b2b3f162c9727ab608f256cc
moscowxy <160858163+moscowxy@users.noreply.github.com>
2025-08-07 18:22:02 +0300

    install_blockassist.sh

--- install_blockassist.sh
+++ install_blockassist.sh
@@ -4,7 +4,7 @@
 
 echo "[1/8] BlockAssist deposu klonlanıyor..."
 git clone https://github.com/gensyn-ai/blockassist.git
-cd blockassist || exit 1
+cd blockassist
 
 echo "[2/8] Setup scripti çalıştırılıyor..."
 ./setup.sh
@@ -21,22 +21,12 @@ source ~/.bashrc
 
 echo "[4/8] Python bağımlılıkları yükleniyor..."
 sudo apt update
-sudo apt install -y make build-essential libssl-dev zlib1g-dev \
-  libbz2-dev libreadline-dev libsqlite3-dev curl git \
-  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
-  libffi-dev liblzma-dev
-
+sudo apt install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl git libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev # Dependencies for Python installation
+pyenv install 3.10
+pip install psutil readchar
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-
-echo "[5/8] Python 3.10 kuruluyor..."
-pyenv install 3.10
-pyenv global 3.10
-
-echo "[6/8] Psutil ve Readchar kuruluyor..."
-pip install --upgrade pip
-pip install psutil readchar
 cd ..
 cd ~/Downloads
 wget https://developer.download.nvidia.com/compute/cudnn/9.11.0/local_installers/cudnn-local-repo-ubuntu2204-9.11.0_1.0-1_amd64.deb
